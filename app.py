f4b6bf369f5692743c92cb0f5fcb7c9359ae72f7
Andrew <aat-78@mail.ru>
2022-11-22 10:47:49 +0300

    реализация дз

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ class MovieSchema(Schema):
     rating = fields.Float()
 
 
-class DirectionSchema(Schema):
+class DirectorSchema(Schema):
     id = fields.Int()
     name = fields.Str()
 
@@ -59,14 +59,14 @@ class GenreSchema(Schema):
 api = Api(app)
 
 movie_ns = api.namespace("movies")
-direction_ns = api.namespace("directions")
-genre_ns = api.namespace("genre")
+director_ns = api.namespace("directors")
+genre_ns = api.namespace("genres")
 
 movie_schema = MovieSchema()
 movies_schema = MovieSchema(many=True)
 
-direction_schema = DirectionSchema()
-directions_schema = DirectionSchema(many=True)
+director_schema = DirectorSchema()
+directors_schema = DirectorSchema(many=True)
 
 genre_schema = GenreSchema()
 genres_schema = GenreSchema(many=True)
@@ -76,9 +76,9 @@ genres_schema = GenreSchema(many=True)
 class MoviesView(Resource):
     def get(self):
         movies_query = db.session.query(Movie)
-        direction_id = request.args.get("direction_id")
-        if direction_id is not None:
-            movies_query = movies_query.filter(Movie.director_id == direction_id)
+        director_id = request.args.get("director_id")
+        if director_id is not None:
+            movies_query = movies_query.filter(Movie.director_id == director_id)
         genre_id = request.args.get("genre_id")
         if genre_id is not None:
             movies_query = movies_query.filter(Movie.genre_id == genre_id)
@@ -99,7 +99,7 @@ class MovieView(Resource):
         movie = db.session.query(Movie).get(uid)
         if not movie:
             return "", 404
-        return movies_schema.dump(movie), 200
+        return movie_schema.dump(movie), 200
 
     def put(self, uid: int):
         update_movie = db.session.query(Movie).filter(Movie.id == uid).update(request.json)
@@ -110,6 +110,84 @@ class MovieView(Resource):
     def delete(self, uid: int):
         delete_movie = db.session.query(Movie).get(uid)
         if delete_movie != 1:
+            return "", 400
+        db.session.delete()
+        db.session.commit()
+        return "", 204
+
+
+@director_ns.route("/")
+class DirectorsView(Resource):
+    def get(self):
+        all_directors = db.session.query(Director)
+        return directors_schema.dump(all_directors), 200
+
+    def post(self):
+        request_json = request.json
+        new_director = Director(**request_json)
+
+        with db.session.begin():
+            db.session.add(new_director)
+
+        return "", 201
+
+
+@director_ns.route("/<int:uid>")
+class DirectorView(Resource):
+    def get(self, uid: int):
+        director = db.session.query(Director).get(uid)
+        if not director:
+            return "", 404
+        return director_schema.dump(director), 200
+
+    def put(self, uid: int):
+        update_director = db.session.query(Director).filter(Director.id == uid).update(request.json)
+        if update_director != 1:
+            return "", 400
+        return "", 204
+
+    def delete(self, uid: int):
+        delete_director = db.session.query(Director).get(uid)
+        if delete_director != 1:
+            return "", 400
+        db.session.delete()
+        db.session.commit()
+        return "", 204
+
+
+@genre_ns.route("/")
+class GenresView(Resource):
+    def get(self):
+        all_genres = db.session.query(Genre)
+        return genres_schema.dump(all_genres), 200
+
+    def post(self):
+        request_json = request.json
+        new_genre = Genre(**request_json)
+
+        with db.session.begin():
+            db.session.add(new_genre)
+
+        return "", 201
+
+
+@genre_ns.route("/<int:uid>")
+class GenreView(Resource):
+    def get(self, uid: int):
+        genre = db.session.query(Genre).get(uid)
+        if not genre:
+            return "", 404
+        return genre_schema.dump(genre), 200
+
+    def put(self, uid: int):
+        update_genre = db.session.query(Genre).filter(Genre.id == uid).update(request.json)
+        if update_genre != 1:
+            return "", 400
+        return "", 204
+
+    def delete(self, uid: int):
+        delete_genre = db.session.query(Genre).get(uid)
+        if delete_genre != 1:
             return "", 400
         db.session.delete()
         db.session.commit()
